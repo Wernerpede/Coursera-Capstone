b6fd5fb6b545325237ece7071577dc83667ee3e4
Wernerpede <gui.werner.007@gmail.com>
2020-08-27 16:12:59 -0300

    Initial Exploratory Data Analysis for Coursera Quiz

--- Scripts/data_processing.py
+++ Scripts/data_processing.py
@@ -4,3 +4,11 @@ import pandas as pd
 import numpy as np
 
 print('Hello Capstone Project Course!')
+
+df = pd.read_csv('Data-Collisions.csv')
+
+columns = df.columns
+
+unknown = df['ROADCOND'].isnull().sum()
+
+correlation = df.corr()
